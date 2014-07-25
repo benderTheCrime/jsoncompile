1a4d1d49a99b11a216e16764c9b504dab8a1a855
jgrosecl49 <joe.groseclose@ordergroove.com>
2014-07-25 17:52:01 -0400

    Added content to script, compiles html files

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
-exports.dist = function() {
-    console.log('hi');
-};
+#! /usr/bin/env node
+(function() {
+    "use strict";
+    var fs = require('fs'),
+        jsonName,
+        htmlName,
+        jsonFile,
+        htmlFile,
+        htmlString = '<!DOCTYPE html>';
+    for (var i = 0; i < process.argv.length; ++i) {
+        var file = process.argv[i];
+        if (file.indexOf('.json') > -1) {
+            jsonName = file;
+        }
+        if (file.indexOf('.html') > -1) {
+            htmlName = file;
+        }
+    }
+    var jsonFile = __dirname + '/' + jsonName,
+        htmlFile = __dirname + '/' + htmlName;
+    fs.readFile(jsonFile, 'utf8', function (err, data) {
+        if (err) {
+            console.log('Error: ' + err);
+            return;
+        }
+        data = JSON.parse(data);
+        htmlString += aggregate(data);
+        fs.writeFile(htmlFile, htmlString, function(err) {
+            if(err) {
+                console.log('Error: ' + err);
+            }
+            else {
+                console.log("The file was saved!");
+            }
+        });
+        // TODO exrpess server to serve page
+    });
+}());
+
+function aggregate(obj) {
+    var keyString = '';
+    for (var key in obj) {
+        var att;
+        if (obj[key]['attributes']) {
+            att = ' ';
+            for (var attribute in obj[key]['attributes']) {
+                att += attribute + '=' + '\''
+                    + obj[key]['attributes'][attribute] + '\'';
+            }
+        }
+        keyString += '<' + key + (att || '') + '>';
+        att = null;
+        if (obj[key]['children']) {
+            keyString += aggregate(obj[key]['children']);
+        }
+        if (obj[key]['content']) {
+            keyString += obj[key]['content'];
+        }
+        keyString += '</' + key + '>';
+    }
+    return keyString;
+}
